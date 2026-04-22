5bb7bfe5a63faed3fa1771cea0b60418a418c653
samman-khanal <samman.samman345@gmail.com>
2026-04-22 10:19:40 +0545

    chore(init): scaffold backend/frontend toolchain and configs

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+formatted the code structure and removed unnecessary 
+lines of code and added comments in the code.
---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: "node",
+  testMatch: ["**/src/tests/**/*.test.js"],
+  testTimeout: 30000,
+  verbose: true,
+  forceExit: true,
+  detectOpenHandles: true
+};
